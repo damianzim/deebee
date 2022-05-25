b051f61524a20117a44b50f72f39163afce3e65f
Damian Zimon <zimondamian@gmail.com>
2022-05-25 22:10:12 +0200

    Python package and venv

---
+++ deebee/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# vim: ts=2 sts=0 sw=2 tw=100 et
+
+from dotenv import load_dotenv
+load_dotenv()
+
+if __name__ == "__main__":
+  print("deebee")
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+# vim: ts=2 sts=0 sw=2 tw=100 et
+
+import setuptools
+
+setuptools.setup(
+  name="deebee",
+  version="0.0.1",
+  description="Interface for a database",
+  url="https://github.com/damianzim/deebee",
+  packages=setuptools.find_packages(),
+)
