c579ca617b0c14b66e15bef662dda52e640047bb
Damian Zimon <zimondamian@gmail.com>
2022-05-31 20:28:36 +0200

    Fix getpass in git bash

--- venv.sh
+++ venv.sh
@@ -5,6 +5,7 @@ if [[ $(uname -s) == MINGW* ]]; then
   source .venv/Scripts/activate
   pip install -r requirements.txt
   pip install -e .
+  alias python='winpty -Xallow-non-tty python.exe'
 else
   python3 -m venv .venv
   source .venv/bin/activate
